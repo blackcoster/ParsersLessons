0d93ab082541c081dbddef09e81fd2af4a62bfdb
Polina Krupenina <krupenina.p@mail.ru>
2023-01-16 15:37:47 +0300

    Урок по переопределению методов

--- Lesson15/5.py
+++ Lesson15/5.py
@@ -1,30 +1,7 @@
-class Базовыематематическиеоперации:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        print("Сложение")
-        return self.x + other.y
-
-    def __sub__(self, other):
-        print("Вычитание")
-        return self.x - other.y
-
-    def __mul__(self, other):
-        print("Умножение")
-        return self.x * other.y
-
-    def __truediv__(self, other):
-        print("Деление")
-        return self.x / other.y
-
-
-
-a = Базовыематематическиеоперации(13, 3)
-b = Базовыематематическиеоперации(33, 333)
-
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
+class Test(str):
+    def __init__(self,word):
+        super().__init__()
+    def __len__(self):
+        return 25
+a = Test('Hello')
+print(len(a))
