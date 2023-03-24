2712408039fbd4d5494e1a1e2f126106c5c0f5dd
Polina Krupenina <krupenina.p@mail.ru>
2023-03-24 16:19:51 +0300

    Работа с Scrapy

--- Lesson31/wikiSpider/wikiSpider/a.py
+++ Lesson31/wikiSpider/wikiSpider/a.py
@@ -4,7 +4,7 @@ from scrapy.linkextractors import LinkExtractor
 
 
 class ArticleSpider(CrawlSpider):
-    name = 'articles'
+    name = 'Sonya'
     allowed_domains = ['wikipedia.org']
     start_urls = ['http://en.wikipedia.org/wiki/Benevolent_dictator_for_life']
     rules = [Rule(LinkExtractor(allow=r'.*'), callback='parse_items', follow=True)]
